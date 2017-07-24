e3f0a872d028a2cfec716fb30a225793a5256305
Barend Erasmus <developersworkspace@gmail.com>
2017-07-24 15:24:53 +0200

    adds compression

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as delay from 'delay';
 import * as readChunk from 'read-chunk';
+import * as zlib from 'zlib';
 
 // Import gateways
 import { DropBoxGateway } from './dropbox-gateway';
@@ -32,46 +33,84 @@ const accessToken = argv.accessToken;
 
 co(function* () {
 
-    const dropBoxGateway = new DropBoxGateway();
-
     const fileName = `${fileNamePrefix}_${moment().format('MM-DD-YYYY-HH-mm-ss')}.Bak`;
+    const compressedFileName = `${fileName}.gz`;
 
-    const sqlConfig = {
-        user: databaseUser,
-        password: databasePassword,
-        server: databaseHost,
-        database: databaseName
-    };
+    yield backupDatabase(databaseHost, databaseUser, databasePassword, databaseName, path.join(filePath, fileName));
 
-    const pool = yield sql.connect(sqlConfig);
-    const sqlResult = yield pool.request().query(`BACKUP DATABASE ${databaseName} TO DISK = '${path.join(filePath, fileName)}'`);
-    pool.close();
+    yield compressFile(path.join(filePath, fileName), path.join(filePath, compressedFileName));
 
-
-    const fileSize = fs.statSync(path.join(filePath, fileName)).size;
-
-    const chunkSize = 100000;
-
-    const sessionId = yield dropBoxGateway.startSession(accessToken);
-
-    for (let i = 0; i < fileSize; i = i + chunkSize) {
-        const buffer = readChunk.sync(path.join(filePath, fileName), i, chunkSize);
-
-        for (let j = 0; j < 5; j++) {
-            try {
-                yield dropBoxGateway.appendSession(accessToken, sessionId, i, buffer);
-                break;
-            } catch (err) {
-                winston.error(err);
-                yield delay(1000);
-            }
-        }
-
-        winston.info(`${i} / ${fileSize}`);
-    }
-
-    yield dropBoxGateway.endSession(accessToken, sessionId, `/${fileName}`, fileSize);
+    yield uploadFile(path.join(filePath, compressedFileName), accessToken);
 
 }).catch((err) => {
     winston.error(err);
 });
+
+function backupDatabase(host: string, user: string, password: string, database: string, filename: string): Promise<void> {
+    return co(function* () {
+        const sqlConfig = {
+            user: user,
+            password: password,
+            server: host,
+            database: database
+        };
+
+        const pool = yield sql.connect(sqlConfig);
+        const sqlResult = yield pool.request().query(`BACKUP DATABASE ${database} TO DISK = '${filename}'`);
+        pool.close();
+
+        return;
+    });
+}
+
+function compressFile(inputFilename: string, outputFilename: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+
+        const gzip = zlib.createGzip();
+        const inputFile = fs.createReadStream(inputFilename);
+        const outputFile = fs.createWriteStream(outputFilename);
+
+        inputFile.pipe(gzip).pipe(outputFile);
+
+        outputFile.on('finish', () => {
+            inputFile.close();
+            inputFile.destroy();
+            outputFile.close();
+            resolve();
+        });
+
+    });
+}
+
+function uploadFile(filename: string, token: string): Promise<void> {
+    return co(function* () {
+
+        const dropBoxGateway = new DropBoxGateway();
+
+        const fileSize = fs.statSync(filename).size;
+
+        const chunkSize = 100000;
+
+        const sessionId = yield dropBoxGateway.startSession(accessToken);
+
+        for (let i = 0; i < fileSize; i = i + chunkSize) {
+            const buffer = readChunk.sync(filename, i, chunkSize);
+
+            for (let j = 0; j < 5; j++) {
+                try {
+                    yield dropBoxGateway.appendSession(token, sessionId, i, buffer);
+                    break;
+                } catch (err) {
+                    winston.error(err);
+                    yield delay(1000);
+                }
+            }
+
+            winston.info(`${i} / ${fileSize}`);
+        }
+
+        yield dropBoxGateway.endSession(accessToken, sessionId, `/${path.basename(filename)}`, fileSize);
+
+        return;
+    });
+}
